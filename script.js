2663c434e0b82cdc94761017c5cc799f5638e977
Piyush Kumar Tiwari <piyusht823@gmail.com>
2021-11-09 21:32:43 +0530

    bugg fix

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Computer Science Student", "Developer", "Freelancer"],
+        strings: ["Computer Science & Engineering Student", "Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
